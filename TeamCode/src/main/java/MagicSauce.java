679ded82c8a795e50bab8aaae2ba1cdcbdbc8296
robotics-magrath <robotics.magrath@gmail.com>
2026-02-06 22:17:57 -0700

    odo start
    we began to fix up the odometry

--- TeamCode/src/main/java/MagicSauce.java
+++ TeamCode/src/main/java/MagicSauce.java
@@ -17,7 +17,7 @@ import java.util.Locale;
 @TeleOp(name="Decode Teleop", group="Linear Opmode")
 public class MagicSauce extends LinearOpMode {
 
-    GoBildaPinpointDriver odo;
+
 
     double oldTime = 0;
     private enum RobotState {
@@ -45,7 +45,7 @@ public class MagicSauce extends LinearOpMode {
     private static final int WRIST_POSITION_SAMPLE = 270;
     private static final int WRIST_POSITION_SPEC = 10;
 
-    private static final int SPINDEXER_TICKS_PER_POSITION = 80;
+    private static final int SPINDEXER_TICKS_PER_POSITION = 85;
 
     private static final double CLAW_POSITION_INIT = 0.9;
     private static final double CLAW_OPEN_POSITION = 0.3;
@@ -56,6 +56,8 @@ public class MagicSauce extends LinearOpMode {
     private int spinpos = 0;
     private double spinoff = 0;
     private int spinWaitTime = 0;
+    private float Xcord = 0;
+    private float Ycord = 0;
 
     private boolean spinWait = false;
 
@@ -63,18 +65,75 @@ public class MagicSauce extends LinearOpMode {
     class Robott {
         long startTime;
         long currentTime;
+        double xoff;
+        double yoff;
+        GoBildaPinpointDriver odo;
 
-        void start() {
+
+        void start(GoBildaPinpointDriver startodo) {
+
+            odo = startodo;
+             /*
+        Set the odometry pod positions relative to the point that the odometry computer tracks around.
+        The X pod offset refers to how far sideways from the tracking point the
+        X (forward) odometry pod is. Left of the center is a positive number,
+        right of center is a negative number. the Y pod offset refers to how far forwards from
+        the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
+        backwards is a negative number.
+         */
+            odo.setOffsets(-84.0, -168.0, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
+
+        /*
+        Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
+        the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
+        If you're using another kind of odometry pod, uncomment setEncoderResolution and input the
+        number of ticks per unit of your odometry pod.
+         */
+            odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+            //odo.setEncoderResolution(13.26291192, DistanceUnit.MM);
+
+
+        /*
+        Set the direction that each of the two odometry pods count. The X (forward) pod should
+        increase when you move the robot forward. And the Y (strafe) pod should increase when
+        you move the robot to the left.
+         */
+            odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+
+        /*
+        Before running the robot, recalibrate the IMU. This needs to happen when the robot is stationary
+        The IMU will automatically calibrate when first powered on, but recalibrating before running
+        the robot is a good idea to ensure that the calibration is "good".
+        resetPosAndIMU will reset the position to 0,0,0 and also recalibrate the IMU.
+        This is recommended before you run your autonomous, as a bad initial calibration can cause
+        an incorrect starting value for x, y, and heading.
+         */
+            //odo.recalibrateIMU();
+            odo.resetPosAndIMU();
+
             startTime = System.currentTimeMillis();
             currentTime = 0;
         }
 
         void update() {
+            odo.update();
             currentTime = System.currentTimeMillis() - startTime;
         }
 
         long getTime() {
             return currentTime;
+        }
+
+        String getPosition() {
+            Pose2D pos = odo.getPosition();
+            return String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+
+        }
+
+        String getVelocity() {
+            return String.format(Locale.US,"{XVel: %.3f, YVel: %.3f, HVel: %.3f}", odo.getVelX(DistanceUnit.MM), odo.getVelY(DistanceUnit.MM), odo.getHeadingVelocity(UnnormalizedAngleUnit.DEGREES));
+
         }
     }
 
@@ -108,57 +167,21 @@ public class MagicSauce extends LinearOpMode {
         Intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         Outake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odowheels");
-
-        /*
-        Set the odometry pod positions relative to the point that the odometry computer tracks around.
-        The X pod offset refers to how far sideways from the tracking point the
-        X (forward) odometry pod is. Left of the center is a positive number,
-        right of center is a negative number. the Y pod offset refers to how far forwards from
-        the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
-        backwards is a negative number.
-         */
-        odo.setOffsets(-84.0, -168.0, DistanceUnit.MM); //these are tuned for 3110-0002-0001 Product Insight #1
-
-        /*
-        Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
-        the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
-        If you're using another kind of odometry pod, uncomment setEncoderResolution and input the
-        number of ticks per unit of your odometry pod.
-         */
-        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
-        //odo.setEncoderResolution(13.26291192, DistanceUnit.MM);
 
 
         /*
-        Set the direction that each of the two odometry pods count. The X (forward) pod should
-        increase when you move the robot forward. And the Y (strafe) pod should increase when
-        you move the robot to the left.
-         */
-        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
-
-
-        /*
-        Before running the robot, recalibrate the IMU. This needs to happen when the robot is stationary
-        The IMU will automatically calibrate when first powered on, but recalibrating before running
-        the robot is a good idea to ensure that the calibration is "good".
-        resetPosAndIMU will reset the position to 0,0,0 and also recalibrate the IMU.
-        This is recommended before you run your autonomous, as a bad initial calibration can cause
-        an incorrect starting value for x, y, and heading.
-         */
-        //odo.recalibrateIMU();
-        odo.resetPosAndIMU();
-
         telemetry.addData("Status", "Initialized");
         telemetry.addData("X offset", odo.getXOffset(DistanceUnit.MM));
         telemetry.addData("Y offset", odo.getYOffset(DistanceUnit.MM));
         telemetry.addData("Device Version Number:", odo.getDeviceVersion());
         telemetry.addData("Heading Scalar", odo.getYawScalar());
-        telemetry.update();
+        telemetry.update();*/
 
         // Wait for the game to start (driver presses START)
         waitForStart();
         resetRuntime();
+        Robott timer = new Robott();
+        timer.start(hardwareMap.get(GoBildaPinpointDriver.class,"odowheels"));
 
 
         // run until the end of the match (driver presses STOP)
@@ -168,7 +191,7 @@ public class MagicSauce extends LinearOpMode {
             Request an update from the Pinpoint odometry computer. This checks almost all outputs
             from the device in a single I2C read.
              */
-            odo.update();
+
 
             /*
             Optionally, you can update only the heading of the device. This takes less time to read, but will not
@@ -177,13 +200,13 @@ public class MagicSauce extends LinearOpMode {
             //odo.update(GoBildaPinpointDriver.ReadData.ONLY_UPDATE_HEADING);
 
 
-            if (gamepad1.a){
+            /*if (gamepad1.a){
                 odo.resetPosAndIMU(); //resets the position to 0 and recalibrates the IMU
             }
 
             if (gamepad1.b){
                 odo.recalibrateIMU(); //recalibrates the IMU without resetting position
-            }
+            }/*
 
             /*
             This code prints the loop frequency of the REV Control Hub. This frequency is effected
@@ -200,15 +223,12 @@ public class MagicSauce extends LinearOpMode {
             /*
             gets the current Position (x & y in mm, and heading in degrees) of the robot, and prints it.
              */
-            Pose2D pos = odo.getPosition();
-            String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
-            telemetry.addData("Position", data);
+            telemetry.addData("Position", timer.getPosition());
 
             /*
             gets the current Velocity (x & y in mm/sec and heading in degrees/sec) and prints it.
              */
-            String velocity = String.format(Locale.US,"{XVel: %.3f, YVel: %.3f, HVel: %.3f}", odo.getVelX(DistanceUnit.MM), odo.getVelY(DistanceUnit.MM), odo.getHeadingVelocity(UnnormalizedAngleUnit.DEGREES));
-            telemetry.addData("Velocity", velocity);
+            telemetry.addData("Velocity", timer.getVelocity());
 
 
             /*
@@ -220,13 +240,13 @@ public class MagicSauce extends LinearOpMode {
             FAULT_X_POD_NOT_DETECTED - The device does not detect an X pod plugged in
             FAULT_Y_POD_NOT_DETECTED - The device does not detect a Y pod plugged in
             FAULT_BAD_READ - The firmware detected a bad I²C read, if a bad read is detected, the device status is updated and the previous position is reported
-            */
+
             telemetry.addData("Status", odo.getDeviceStatus());
 
             telemetry.addData("Pinpoint Frequency", odo.getFrequency()); //prints/gets the current refresh rate of the Pinpoint
             telemetry.addData("REV Hub Frequency: ", frequency); //prints the control system refresh rate
             telemetry.update();
-
+            */
 
 
         telemetry.addData("Status", "Initialized");
@@ -234,13 +254,11 @@ public class MagicSauce extends LinearOpMode {
 
         waitForStart();
 
-        Robott timer = new Robott();
-        timer.start();
 
         //allows a wait before you can press the button again
         if (gamepad2.x) {
             if (spinWait == true) {
-                spinpos = (spinpos + 1) ;
+                spinpos = (spinpos + 2) ;
                 spinWaitTime = 30;
                spinWait = false;
             }
@@ -274,17 +292,17 @@ public class MagicSauce extends LinearOpMode {
 
             double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(turn), 1);
 
-            Leftfw.setPower((forward + strafe + turn) / denominator);
-            Leftbw.setPower((forward - strafe + turn) / denominator);
-            Rightfw.setPower((forward - strafe - turn) / denominator);
-            Rightbw.setPower((forward + strafe - turn) / denominator);
+            Leftfw.setPower((forward - strafe + turn) / denominator);
+            Leftbw.setPower((forward - strafe - turn) / denominator);
+            Rightfw.setPower((forward + strafe - turn) / denominator);
+            Rightbw.setPower((forward + strafe + turn) / denominator);
 
             // --- Spindexer Control ---
             spinoff = spinoff + gamepad2.left_stick_x;
 
             spindexer.setTargetPosition(spinpos * SPINDEXER_TICKS_PER_POSITION + (int) spinoff);
 
-                spindexer.setPower(0.5);
+                spindexer.setPower(0.75);
 
             // --- Arm/Output movement ---
             Outake.setPower(targetOutake);
